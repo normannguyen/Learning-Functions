d479fec105f5e96842cf00beb9d94183c33ea3c1
normannguyen <47510789+normannguyen@users.noreply.github.com>
2019-03-26 20:42:57 -0700

    Update 1

---
+++ Learning-Functions/Learning-Functions.cpp
@@ -0,0 +1,54 @@
+//Norman Nguyen
+//Program: Learning Functions with Interactive Fiction
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+//Get Text from the user input
+void WelcomeUser()
+{
+	cout << "Welcome to the new version of The (Unaccurate) Storytelling!\n\n";
+	cout << "Much like the first game, you will be entering text from your choosing and it will print out the story." << endl;
+}
+//Get Text from the user input
+string GetUserText()
+{
+	system("pause");
+	return 0;
+}
+//Get Number from the user input
+int GetUserNumber()
+{
+	system("pause");
+	return 0;
+}
+//Get Name from the user input
+string GetUserName()
+{
+	system("pause");
+	return 0;
+}
+void LineFormat()
+{
+	cout << "\n\n";
+	cout << "________________________________________________\n\n";
+	cout << "________________________________________________\n\n";
+	cout << "\n\n";
+}
+//Main Function
+int main()
+{
+	WelcomeUser();
+	LineFormat();
+	system("pause");
+
+
+	LineFormat();
+	system("pause");
+
+
+	system("pause");
+	return 0;
+}
